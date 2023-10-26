ff1d794e3b78e628c46d1652236b8d7ae3a5defa
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-10-26 12:29:57 +0200

    add_category form added

---
+++ add_category.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Category</title>
+</head>
+<body>
+    <form>
+        Category: <br>
+        <input type="text" name="category_name"> <br> <br>
+        Category: <br>
+        <input type="date" name="category_entry_date"> <br> <br>
+        <input type="submit" value="submit">
+    </form> 
+</body>
+</html>
